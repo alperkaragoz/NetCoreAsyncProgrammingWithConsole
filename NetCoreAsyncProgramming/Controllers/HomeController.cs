d60dd3cd0fec2a0c8ab6ad2b6767f67203da6f21
Alper Karagöz <alperkaragoz@outlook.com>
2023-04-11 22:36:54 +0300

    Add project files.

---
+++ NetCoreAsyncProgramming/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+//using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace NetCoreAsyncProgramming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+
+        public async Task<IActionResult> GetContentAsync()
+        {
+
+            // ------- METHOD 1- ContinueWith ile ------->
+
+            // GetStringAsync metodundan sonra ContinueWith içerisindeki kod bloğu çalışacaktır.
+            // GetStringAsync metodundan dönen data Task<string> tipinde olduğundan dolayı ContinueWith içerisindeki "data" 'da Task<string> tipinde olmaktadır.
+            var task = new HttpClient().GetStringAsync("https://www.google.com")
+                .ContinueWith((data) =>
+                {
+                    Debug.Write($"Google Length: {data.Result.Length}");
+                });
+
+            // Eğer ContinueWith içerisinde çok fazla process/kod var ise alternatif olarak geriye Task<string> tipinde değer dönen bir metod yazarak daha clean hale getirebiliriz. Örn: Run metodu.
+            // Örnek kullanımı
+            var task2 = new HttpClient().GetStringAsync("https://www.google.com")
+                .ContinueWith(Run);
+
+            // Önce alt satır çalışacak.Sonra task içerisindeki işlemler çalışacak.Çünkü await keywordünü en alttaki satırda kullanıyoruz.
+            Debug.Write("Other Processes");
+
+            await task;
+
+            //// await kullandığımızda datayı istiyoruz.
+            //var data = await task;
+
+
+            // ------ METHOD 1- ContinueWith olmadan ------>
+
+
+            // Bu durumda önce 
+            var taskWithoutContinueWith = new HttpClient().GetStringAsync("https://www.google.com");
+
+            Debug.Write("Other Processes Without ContinueWith");
+
+            var dataWithoutContinueWith = await taskWithoutContinueWith;
+            Debug.Write($"Google Length: {dataWithoutContinueWith.Length}");
+
+
+            return Ok(task);
+        }
+
+        public static void Run(Task<string> data)
+        {
+            Debug.Write($"Google Length: {data.Result.Length}");
+        }
+    }
+}
---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+
+internal class Program
+{
+    private async static Task Main(string[] args)
+    {
+        Console.WriteLine($"Main Thread: {Thread.CurrentThread.ManagedThreadId}");
+
+        List<string> urlList = new()
+            {
+                "https://www.google.com",
+                "https://www.microsoft.com",
+                "https://www.amazon.com"
+            };
+
+        List<Task<Content>> tasks = new();
+
+        urlList.ToList().ForEach(url =>
+        {
+            tasks.Add(GetContentAsync(url));
+        });
+
+        // Eğer await kullanmamış olsaydık Task<Content> alacaktı contents objesi.await keywordü ile beraber Content[] alıyoruz.
+        var contents = await Task.WhenAll(tasks.ToArray());
+
+        contents.ToList().ForEach(content =>
+        {
+            Console.WriteLine($"{content.Site} Length:{content.Length}");
+        });
+    }
+    public static async Task<Content> GetContentAsync(string url)
+    {
+        Content c = new();
+        var data = await new HttpClient().GetStringAsync(url);
+
+        c.Site = url;
+        c.Length = data.Length;
+        Console.WriteLine($"GetContentAsync thread: {Thread.CurrentThread.ManagedThreadId}");
+
+        return c;
+    }
+    public class Content
+    {
+        public string Site { get; set; }
+        public int Length { get; set; }
+    }
+}
