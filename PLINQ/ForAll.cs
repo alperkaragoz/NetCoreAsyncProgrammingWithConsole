e5654e0088fc76cf2d26510406c6c3ad6dc4d95d
Alper Karagöz <alperkaragoz@outlook.com>
2023-04-20 22:43:19 +0300

    PLINQ - ForAll Sample

---
+++ PLINQ/ForAll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLINQ
+{
+    public class ForAll
+    {
+        public static void ForAllSample()
+        {
+            var array = Enumerable.Range(1, 100).ToList();
+
+            // Mod'u sıfır olanları getiriyoruz.(2'ye bölünebilen sayılar)
+            var newArray = array.AsParallel().Where(x => x % 2 == 0);
+
+            // Eğer PLINQ kullanıyorsak ve gelen datadaki sonuçları görmek istiyorsak normal foreach kullanmak performans anlamında iyi değildir.Bunun yerine ForAll metodunu kullanıyoruz.
+            newArray.ForAll(x =>
+            {
+                Thread.Sleep(500);
+                Console.WriteLine(x);
+            });
+        }
+    }
+}
--- PLINQ/Program.cs
+++ PLINQ/Program.cs
@@ -1,6 +1,9 @@
 ﻿using static PLINQ.AsParallel;
+using static PLINQ.ForAll;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-AsParallelSample();
+//AsParallelSample();
+
+ForAllSample();
